43e48922a0b9db5f81a0aa68aaa2f87bf98428bb
Parthivm <parthivm2004@gmail.com>
2024-10-01 22:02:01 +0530

    1/10/24

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-const con=document.getElementById("container");
-let row=document.getElementsByClassName("gridrows");
-let cells=document.getElementsByClassName("cells");
+const con = document.getElementById("container");
 
-function makegrid(){
-    makerows(16);
-    makecells(16);
-}
-
-function makerows(rn){
-    for(i=0;i<rn;i++){
-        let row=document.createElement("div");
-        con.appendChild(row).className="gridrows";
-    }
-}
-
-function makecells(cn){
-    for(i=0;i<row.length;i++){
-        for(j=0;j<cn;j++){
-            let ncell=document.createElement("div");
-            row[j].appendChild(ncell).className="cells";
+        function makegrid(rows, cols) {
+            for (let i = 0; i < rows * cols; i++) {
+                let cell = document.createElement("div"); 
+                cell.className = "cells";
+                con.appendChild(cell); 
+            }
         }
-    }
-}
+
+        makegrid(16, 16); 
